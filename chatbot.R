8da86fc84edc7fc73b1211edf4bb076d58c32afc
Broen Kjeka <ekb@ssb.no>
2022-07-27 12:24:35 +0200

    added some new commands

--- chatbot.R
+++ chatbot.R
@@ -33,8 +33,8 @@ add_new_input <- function(char){
     score_matrix <<- cbind(score_matrix, 1)
     phrases <<- c(phrases, char)}
 
-# delete output
-remove_last_output <- function(char){
+# delete phrase
+remove_phrase <- function(char){
     idx <- which(phrases==char)
     score_matrix <<- score_matrix[-idx, -idx]
     phrases <<- phrases[-idx]
@@ -88,13 +88,45 @@ command_mode <- function(){
         print("1: print current output")
         print("2: delete last output")
         print("3: get stats")
+        print("4: check if phrase is saved")
+        print("5: check likely output of input")
+        print("6: check input for likely output")
+        print("7: delete specific phrase")
     }
     else if(cmd == "1"){print(output)}
-    else if(cmd == "2"){remove_last_output(output)
+    else if(cmd == "2"){remove_phrase(output)
         print(paste("Removed", output))
         output <<- ""
     }
     else if(cmd == "3"){run_stats()}
+    else if(cmd == "4"){check_phrase  <- readline(prompt = "type phrase: ")
+        if(check_phrase %in% phrases){
+            print(paste("Phrase is in position", which(phrases==check_phrase)))
+        }
+        else{print("Phrase is not saved")}
+    }
+    else if(cmd == "5"){check_phrase  <- readline(prompt = "type phrase: ")
+        if(check_phrase %in% phrases){
+            score_vector <- score_matrix[phrases == check_phrase,]
+            print(phrases[score_vector > 1])
+        }
+        else{print("Phrase is not saved")}
+    }
+    else if(cmd == "6"){check_phrase  <- readline(prompt = "type phrase: ")
+        if(check_phrase %in% phrases){
+            score_vector <- score_matrix[,phrases == check_phrase]
+            print(phrases[score_vector > 1])
+        }
+        else{print("Phrase is not saved")}
+    }
+    else if(cmd == "7"){phrase_to_remove <<- readline(prompt = "type phrase: ")
+        if(phrase_to_remove %in% phrases){
+            remove_phrase(phrase_to_remove)
+            print(paste("Removed", phrase_to_remove))
+            output <<- ""
+        }
+        else{print("Phrase is not saved")}
+    }
     else(print("command unknown"))
     }
 }
