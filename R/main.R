2bf59f65512798e0dc2982a1a505528523b57922
Vespertine13 <eivind.kb@hotmail.com>
2023-08-20 10:39:22 +0200

    stable and works in both training and elisp

--- R/main.R
+++ R/main.R
@@ -22,6 +22,13 @@ run_stats <- function(){
     print(nrow(score_matrix))
     print("ncol score_matrix")
     print(ncol(score_matrix))
+    print("mean score:")
+    print("Topic Matrix")
+    print(mean(topic_score_matrix))
+    print("nrow score_matrix:")
+    print(nrow(topic_score_matrix))
+    print("ncol score_matrix")
+    print(ncol(topic_score_matrix))
     print("Number of phrases:")
     print(length(phrases))
 }
@@ -48,8 +55,15 @@ remove_phrase <- function(char){
     phrases <<- phrases[-idx]
 }
 
+# calculate score
+calculate_score <- function(input, output_last){
+    score <- score_matrix[phrases == input, ]
+    topic_score <- topic_score_matrix[phrases == output_last, ]
+    return(score + topic_score)
+}
+    
 # give feedback
-give_feedback <- function(input, output){
+give_feedback <- function(input, output, input_last){
     # add +1 to the response written by the user
     score_matrix[phrases == output, phrases == input] <<- score_matrix[phrases == output, phrases == input] + 1
     topic_score_matrix[phrases == input_last, phrases == input] <<- topic_score_matrix[phrases == input_last, phrases == input] + 1
@@ -61,13 +75,9 @@ new_phrase <- function(){
         phrases, split=" "))), sample(1:10, 1)), collapse=" "))
 }
 
-
 # selects a responce from the matrix
 # select from all over 0
-select_from_all <- function(input){   
-    score <- score_matrix[phrases == input, ]
-    topic_score <- topic_score_matrix[phrases == output_last, ]
-    score <- score + topic_score
+select_from_all <- function(score){
     score[score<0] <- 0
     sample_lst <- c()
     for(i in 1:length(score)){
@@ -77,10 +87,7 @@ select_from_all <- function(input){
 }
 
 # select from all over 1
-advance_select <- function(input){
-    score <- score_matrix[phrases == input, ]
-    topic_score <- topic_score_matrix[phrases == output_last, ]
-    score <- score + topic_score
+advance_select <- function(score){
     score[score<2] <- 0
     sample_lst <- c()
     for(i in 1:length(score)){
@@ -102,9 +109,9 @@ jaccard_select <- function(input){
     results <- sapply(strsplit(phrases," "), jaccard, splitted_input)
     results[results == max(results)] <- -1
     jaccard_input <<- sample(phrases[which(results == max(results))], 1)
-    score <- score_matrix[phrases == jaccard_input, ]
-    if(sum(score>1)>0){
-        jaccard_output <<- advance_select(jaccard_input)
+    score_jac <- score_matrix[phrases == jaccard_input, ]
+    if(sum(score_jac>1)>0){
+        jaccard_output <<- advance_select(score_jac)
         if(jaccard_output == input){
             return(jaccard_input)
         }        
@@ -206,7 +213,7 @@ run_training <- function(){
     selection_log <<- c("start_log")
     output_log <<- c("start_log")
     # set start input and output values
-    output <<- readLines(paste0(PATH, "output.txt"))
+    output <<- "talk to me!"
     input <<- readLines(paste0(PATH, "input.txt"))
     if (length(output) == 0){output <- ""}
     if (length(input) == 0){input <- ""}
@@ -227,8 +234,10 @@ run_training <- function(){
 
         # adds the word if it isn't already in phrases
         if(!(input %in% phrases)){add_new_input(input)}
-        give_feedback(input, output)
-
+        give_feedback(input, output, input_last)
+        
+        # calculate score
+        score <<- calculate_score(input, output_last)
         # adds a new random phrase, 1% chance
         if(sample(c(T, rep(F, 99)), 1)){
             current_selection <<- "random phrase"
@@ -238,13 +247,13 @@ run_training <- function(){
         # selects from all phrases over value 0, 1% chance
         else if(sample(c(T, rep(F, 99)), 1)){
             current_selection <<-  "all over 0 phrases"
-            output <<- select_from_all(input)
+            output <<- select_from_all(score)
         }
 
         # selects from all phrases over value 1
-        else if(sum(score_matrix[phrases == input, ]>1)>0){
+        else if(sum(score>1)>0){
                 current_selection <<- "all over 1 phrases"
-                output <<- advance_select(input)
+                output <<- advance_select(score)
         }
         
         # selects with Jaccard similarity
@@ -254,18 +263,18 @@ run_training <- function(){
         }
         if(answers_q_with_q(input, output)){
             current_selection <<- "all over 0 phrases"
-            output <<- select_from_all(input)
+            output <<- select_from_all(score)
         }
         
         # checks if bot repeats you
         if(bot_repeats(output, input)){
             current_selection <<- "all over 0 phrases"
-            output <<- select_from_all(input)
+            output <<- select_from_all(score)
         }
         # checks if bot repeats herself
         if(bot_repeats(output, output_log[length(output_log)])){
             current_selection <<- "all over 0 phrases"
-            output <<- select_from_all(input)
+            output <<- select_from_all(score)
         }
 
         
@@ -277,6 +286,7 @@ run_training <- function(){
     print("Saving data...")
     # save data
     arrow::write_parquet(as.data.frame(score_matrix), paste0(PATH, "chatbot_matrix.parquet"))
+    arrow::write_parquet(as.data.frame(topic_score_matrix), paste0(PATH, "chatbot_topic_matrix.parquet"))
     write.csv(phrases, paste0(PATH, "chatbot_phrases.csv"))
     print("Chatbot left")
 }
@@ -284,38 +294,48 @@ run_training <- function(){
 # run parla for use with elisp function 
 # it is similar to run_training()
 run_parla <- function(input){
-    input <- tolower(input)
+    # load data
+    score_matrix <<- unname(as.matrix(arrow::read_parquet(paste0(PATH, "chatbot_matrix.parquet"))))
+    topic_score_matrix <<- unname(as.matrix(arrow::read_parquet(paste0(PATH, "chatbot_topic_matrix.parquet"))))
+    phrases <<- as.character(unlist(read.csv(paste0(PATH, "chatbot_phrases.csv"))[-1]))
     output <<- readLines(paste0(PATH, "output.txt"))
     input_last <<- readLines(paste0(PATH, "input.txt"))
     if (length(output) == 0){output <- ""}
     if (length(input_last) == 0){input_last <- ""}
     output_last <- output
-    score_matrix <<- unname(as.matrix(arrow::read_parquet(paste0(PATH, "chatbot_matrix.parquet"))))
-    phrases <<- as.character(unlist(read.csv(paste0(PATH, "chatbot_phrases.csv"))[-1]))
+    # treat input
+    input <- tolower(input)
+    # check if input already exist, if not add to phrases
     if(!(input %in% phrases)){add_new_input(input)}
-    give_feedback(input, output)
+    # give scores
+    give_feedback(input, output, input_last)
 
+    # calculate score of current input and last output pair
+    score <<- calculate_score(input, output_last)
+    # selection of new output
+    # random phrase
     if(sample(c(T, rep(F, 99)), 1)){
         output <- new_phrase()
     }
     else if(sample(c(T, rep(F, 99)), 1)){
-        output <- select_from_all(input)
+        output <- select_from_all(score)
     }
-    else if(sum(score_matrix[phrases == input, ]>1)>0){
-        output <- advance_select(input)
+    else if(sum(score>1)>0){
+        output <- advance_select(score)
     }
     else{
         output <- jaccard_select(input)
     }
     if(answers_q_with_q(input, output)){ #check is bot answers a question with a question (should be rare but not impossible)
-        output <- select_from_all(input)
+        output <- select_from_all(score)
     }
     if(bot_repeats(output, output_last)){
-        output <- select_from_all(input)
+        output <- select_from_all(score)
         }
     if(bot_repeats(output, input)){ #check is bot repeats what you said (should be rare but not impossible)
-        output <- select_from_all(input)
+        output <- select_from_all(score)
     }
+    # save updated data
     arrow::write_parquet(as.data.frame(score_matrix), paste0(PATH, "chatbot_matrix.parquet"))
     arrow::write_parquet(as.data.frame(topic_score_matrix), paste0(PATH, "chatbot_topic_matrix.parquet"))
     write.csv(phrases, paste0(PATH, "chatbot_phrases.csv"))
